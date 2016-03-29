6ae3e4d27dcc78c4506a19188680b3f065691169
superbean <www.zhouze.1996@qq.com>
2016-03-29 13:35:36 +0800

    Integrate LoginUI code.

---
+++ AuctionClient/java/com/example/bean/auctionclient/ForgetPwdActivity.java
@@ -0,0 +1,20 @@
+package com.example.bean.auctionclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ForgetPwdActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pwd);
+    }
+
+}
+
+
